16397c58ed6b74e23dddfe33f5c9ef913cd0b53e
L3oN <leon_o@gmx.de>
2023-11-16 20:44:05 +0100

    created map for bikethefts in 2023

--- add_multipolygons_and_create_map.py
+++ add_multipolygons_and_create_map.py
@@ -3,16 +3,17 @@ tqdm.pandas()
 from utils import *
 
 # load new data to add geodata to
+YEAR = 2023
 new_data_path ='data/2022-2023_bikethefts/'
 load_file_name = '2022-2023_bikethefts_preprocessed.csv'
-save_file_name = '2022_bikethefts_map.html'
+save_file_name = f'{YEAR}_bikethefts_map.html'
 
 df = open_new_data(new_data_path=new_data_path+load_file_name, PLR_ID_column_name='LOR', encoding='ISO-8859-1')
 
 # TODO transform new data to effectively add geodata (e.g. group by PLR_ID and/or timestamp before adding geodata to each row)
 # groupby PLR_ID and year
 df_thefts_per_year = df.groupby(['PLR_ID', 'Year']).size().reset_index(name='Bike_Thefts').sort_values('Year')
-df_thefts_2022 = df_thefts_per_year[df_thefts_per_year['Year'] == 2022]
+df_thefts_2022 = df_thefts_per_year[df_thefts_per_year['Year'] == YEAR]
 
 # load geojson containing geodata
 geojson_path = 'data/multipolygons/lor_planungsraeume_2021.geojson'
