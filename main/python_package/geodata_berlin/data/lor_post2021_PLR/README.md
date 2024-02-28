3a199a68f1ad3e7de07b12055eb7fea48f62bd21
L3oN <Leon_O@gmx.de>
2024-02-28 14:54:25 +0100

    added LOR PLR data from ODIS including README.md and adapted LookupTableManager

---
+++ main/python_package/geodata_berlin/data/lor_post2021_PLR/README.md
@@ -0,0 +1,5 @@
+content: "Dargestellt werden die aktuellen, seit 2021 verwendeten, 542 Planungsräume, die die unterste Beobachtungs- und Planungsebene der lebensweltlich orientierten Raumhierarchie (LOR) im RBS abbilden. Durchschnittlich umfaßt ein Planungsraum 7.500 Einwohner. Mehr Infos finden Sie hier: https://www.stadtentwicklung.berlin.de/planen/basisdaten_stadtentwicklung/lor/
+Original WFS URL: https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_lor_plr_2021" - Quote from source
+shape: (542, 6)  
+columns: ['PLR_ID', 'PLR_NAME', 'BEZ', 'STAND', 'GROESSE_M2', 'geometry']  
+source: (28.02.2024) https://daten.odis-berlin.de/de/dataset/lor_planungsgraeume_2021/
--- main/python_package/geodata_berlin/geodata_berlin.py
+++ main/python_package/geodata_berlin/geodata_berlin.py
@@ -69,12 +69,33 @@ class LookupTableManager:
 
                 self.id_column = 'PLR_NAME'
                 self.return_columns_map = {'id':'PLR_ID', 'geometry':'geometry'}
+        elif resolution_mode == 'LOR_PLR_odis':
+            # self.lookup_table_df = gpd.read_file(data_path + 'lor_shp_2021/lor_plr.shp')
+            # TODO fix duplicate PLR_NAME for "Schloßstraße" in the shapefile, then remove self.lookup_table_df declaration in the following if statements
+            if map_by == 'ID':
+
+                ### remove when duplicate todo is fixed
+                self.lookup_table_df = gpd.read_file(data_path + 'lor_post2021_PLR/lor_planungsraeume_2021.shp')
+                ###
+
+                self.id_column = 'PLR_ID'
+                self.return_columns_map = {'name':'PLR_NAME', 'district':'BEZ', 'state':'STAND', 'size_m2':'GROESSE_M2', 'geometry':'geometry'}
+            elif map_by == 'NAME':
+
+                ### remove when duplicate todo is fixed
+                warnings.warn("For now the PLR IDs '06100102' & '04300414' ('Schloßstraße') will return NaN due to a pending bug fix!", UserWarning)
+                lookup_table_df_duplicates = gpd.read_file(data_path + 'lor_post2021_PLR/lor_planungsraeume_2021.shp')
+                self.lookup_table_df = lookup_table_df_duplicates[~lookup_table_df_duplicates.duplicated(subset='PLR_NAME', keep=False)]
+                ###
+
+                self.id_column = 'PLR_NAME'
+                self.return_columns_map = {'id':'PLR_ID', 'district':'BEZ', 'state':'STAND', 'size_m2':'GROESSE_M2', 'geometry':'geometry'}
             else:
                 raise ValueError(f"Invalid mapping type: '{map_by}' - must be one of 'ID' or 'NAME'!")
         else:
-            raise ValueError(f"Invalid Resolution Mode: '{resolution_mode}' - must be one of 'PLZ', 'LOR_BZR', 'LOR_PGR' or 'LOR_PLR'")
+            raise ValueError(f"Invalid Resolution Mode: '{resolution_mode}' - must be one of 'PLZ', 'LOR_BZR', 'LOR_PGR', 'LOR_PLR' or 'LOR_PLR_odis'!")
         
-    def get_meta_data(self, input_df, id_col, exclude_column=[], df_type='geopandas'):
+    def get_meta_data(self, input_df, id_col, exclude_column=['district', 'state'], df_type='geopandas'):
         """
 
         """
@@ -88,7 +109,7 @@ class LookupTableManager:
             raise ValueError(f"exclude_column must be a list, not a '{type(exclude_column)}'!")
         if not all(isinstance(col, str) for col in exclude_column):
             raise ValueError(f"exclude_column must be a list of strings, containing 'geometry', 'name' or 'id'!")
-        if not all(col in ['geometry', 'name', 'id'] for col in exclude_column):
+        if not all(col in ['geometry', 'name', 'id', 'district', 'state'] for col in exclude_column):
             raise ValueError(f"exclude_column must be a list of strings, containing 'geometry', 'name' or 'id'!")
         if len(exclude_column) != len(set(exclude_column)):
             raise ValueError(f"exclude_column must not contain duplicate values!")
@@ -96,15 +117,14 @@ class LookupTableManager:
             raise ValueError(f"ID Column '{id_col}' must contain only string values!")
 
         # drop column from returned columns if it is in exclude_column
-        if exclude_column:
-            return_columns = [self.return_columns_map[col] for col in self.return_columns_map if col not in exclude_column]
-        else:
-            return_columns = list(self.return_columns_map.values())
+        # k == column name in returned df
+        # v == column name in original lookuptable
+        return_columns = {k: v for k, v in self.return_columns_map.items() if k not in exclude_column}
 
         # Map IDs to meta data
         df = input_df.copy()
-        for col in return_columns:
-            df[col] = input_df[id_col].map(self.lookup_table_df.set_index(self.id_column)[col])
+        for column_name_in_returned_df, column_name_in_lookuptable in return_columns.items():
+            df[column_name_in_returned_df] = input_df[id_col].map(self.lookup_table_df.set_index(self.id_column)[column_name_in_lookuptable])
 
         # Convert the updated pandas DataFrame to a GeoDataFrame
         if df_type == 'geopandas':
